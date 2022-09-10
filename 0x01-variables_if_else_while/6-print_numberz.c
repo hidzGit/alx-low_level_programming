942ce0d5400dd54a8552d76a83c0f6d9a9d32181
Idris Oladosu <idrisoladosu1@gmail.com>
2022-09-10 13:17:11 -0700

    add '' to num in 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Return: 1..10
+ */
+int main(void)
+{
+	int numbers = '0';
+
+	while (numbers <= '9')
+	{
+	putchar(numbers);
+	numbers++;
+	}
+	putchar('\n');
+	return (0);
+}
