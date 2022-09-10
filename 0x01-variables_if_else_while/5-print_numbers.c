0a7b4781390f4bf63bf3422087e1ce738e31cb44
Idris Oladosu <idrisoladosu1@gmail.com>
2022-09-10 13:07:15 -0700

    add c that prints 0...9 as 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Return: 1..10
+ */
+int main(void)
+{
+	int numbers = 0;
+
+	while (numbers <= 9)
+	{
+	printf("%d", numbers);
+	numbers++;
+	}
+	return (0);
+}
