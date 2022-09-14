6a8d59cc447104158a69273910e802f6df860de8
Idris Oladosu <idrisoladosu1@gmail.com>
2022-09-14 11:29:50 +0000

    task 01

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * main - print alphabets using a void function
+ * print_alphabet - print alphabets using a void function
  *
  * Return: 0
  */
 void print_alphabet(void)
 {
 	int alpha = 'a';
-	
-	while(alpha <= 'z')
+
+	while (alpha <= 'z')
 	{
 	_putchar(alpha);
 	alpha++;
