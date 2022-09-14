4363e7a7f8201b2afe51b0fc67ecc075f146329b
Idris Oladosu <idrisoladosu1@gmail.com>
2022-09-14 18:53:56 +0000

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,11 +3,10 @@
 /**
  * print_last_digit - print input number last digit;
  * @x: input digit;
- * @y: last digit of input;
  * Return: 0
  */
 int print_last_digit(int x)
 {
 	_putchar(x % 10 + '0');
-	return (_putchar(x % 10 + '0'));
+	return (0);
 }
