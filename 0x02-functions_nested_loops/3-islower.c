018fb31453181a5cbce432f9f781456194e64a5b
Idris Oladosu <idrisoladosu1@gmail.com>
2022-09-14 14:47:08 +0000

    Task 03
    add betty docs to task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
+/**
+ * _islower - check if the input character is lowercase or not
+ * @c: character inputted
+ * Return: 1 if it is a lowercase, 0 if not
+ */
 int _islower(int c)
 {
 
- if (c >= 'a' && c <= 'z')
-	 return (1);
- else
-	 return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 
 }
