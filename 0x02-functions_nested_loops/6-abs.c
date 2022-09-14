9fd494c49eb4d48da98686c57141aaf83c6e2735
Idris Oladosu <idrisoladosu1@gmail.com>
2022-09-14 17:59:06 +0000

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,18 @@
 #include "main.h"
 
 /**
+ * _abs - print absolute value of int
+ * @x: integer input
+ * Return: 0
  */
 int _abs(int x)
 {
-	if(x >= 0)
+	if (x >= 0)
 		_putchar('x');
-	if(x =< 0)
-
+	else
+	{
+		x *= -1;
+		_putcher('x')
+	}
+return (0);
 }
