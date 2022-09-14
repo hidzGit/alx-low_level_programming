e41820ea7212016951a594dc13ff29909b87f0fc
Idris Oladosu <idrisoladosu1@gmail.com>
2022-09-14 00:28:12 +0000

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print _putchar using puchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int y;
+char x[8] = "_putchar";
+y = 0;
+while(y < 8)
+{
+putchar(x[y]);
+y++;
+}
+putchar('\n');
+ return(0);
+}
