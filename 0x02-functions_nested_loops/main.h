14c6fbb7175625b9dd6adecf948242277e1dc735
Idris Oladosu <idrisoladosu1@gmail.com>
2022-09-14 08:54:42 +0000

    task 01 add _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,6 @@
-#include <unistd.h>
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 
-/**
- *  * _putchar - writes the character c to stdout
- *   * @c: The character to print
- *    *
- *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
- *       */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
+int _putchar(char);
+
+#endif
