4d78bda19ae79cdddcb746ed1f91f5f449ad54d7
Idris Oladosu <idrisoladosu1@gmail.com>
2022-09-15 08:12:55 +0000

    Task 0
    add 0-main.c, main.h and 0-positive... func file

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - function that confirms negative and positive numbers
+ *
+ * @if statement to test if a number is greater than zero
+ *
+ * Return: the number is neg/positive/zero
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,3 @@
+This directory contains debuging projects on C
+
+1 - Test for a main file
