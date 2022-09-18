cce1fede1e5bcec7e2cd7e2c7bb475d30e8efdce
Idris Oladosu <idrisoladosu1@gmail.com>
2022-09-18 18:20:01 +0000

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - print #in a triangular manner
+ * @size: number of # to make up the triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+	
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	
+	{
+		for (j = 1; j <= size; j++)
+		{
+			for (i = size - j; i >= 1; i--)
+				_putchar(' ');
+
+			for (i = 1; i <= j; i++)
+				_putchar('#');
+
+			_putchar('\n');
+		}
+	}
+}
