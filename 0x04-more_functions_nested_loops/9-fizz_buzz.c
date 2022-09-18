32eb099acc3ecf23ec6fba74db11a53b4f33b14d
Idris Oladosu <idrisoladosu1@gmail.com>
2022-09-18 07:22:22 +0000

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - print 1..100, prints fizz at
+ * 3 and it multiples,
+ * buzz at 5 and it multiples
+ * fizzbuzz at the multiples of 5*3
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			printf("Fizz");
+			putchar(' ');
+			continue;
+		}
+		if ((i % 5) == 0)
+		{
+			printf("Buzz");
+			putchar(' ');
+			continue;
+		}
+		if ((i % 15) == 0)
+		{
+			printf("FizzBuzz");
+			putchar(' ');
+			continue;
+		}
+		printf("%d", i);
+		printf(" ");
+	}
+	putchar('\n');
+	return (0);
+}
