5c7333d42ef805d4dd0f32bcce1c145a8ea45432
Idris Oladosu <idrisoladosu1@gmail.com>
2022-09-20 22:07:20 +0000

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,11 +1,28 @@
-#include "main"
+#include "main.h"
+
+/**
+ * puts_half - starts printing from halfway a string
+ * @str: string pointer
+ */
 
 void puts_half(char *str)
 {
-	n = _strlen(*str);
-	
-	int num;
-	
-	if (n % 2 == 0)
+	int length = 1;
+	int mnum;
+	int mmnum;
+
+	while (str[length] != '\0')
 	{
-		for (num = (n % 2); num >= ()  )
+		length++;
+	}
+	if (length % 2 == 0)
+	{
+		for (mnum = length / 2; mnum <= length; mnum++)
+			_putchar(str[mnum]);
+	}
+	else
+	{
+		for ((mmnum = (length - 1) / 2); mmnum <= length; mmnum++)
+			_putchar(str[mmnum]);
+	}
+}
