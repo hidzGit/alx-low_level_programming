b39620badf70fd95b9003a30cc0ad2f8630f3149
Idris Oladosu <idrisoladosu1@gmail.com>
2022-09-26 17:44:26 -0700

    pointer and array

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - check for the first occurence of a char
+ * in a string and returns the pointerto it
+ * @s: string to check
+ * @c: char to check for
+ * Return: pointer if char is present, null if not
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *ans;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			ans = &s[i];
+			return (ans);
+		}
+	}
+	return (NULL);
+}
