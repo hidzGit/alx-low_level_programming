b2418b15bef42981dee4cc1e83e949878a81cb5a
Idris Oladosu <idrisoladosu1@gmail.com>
2022-09-29 03:23:07 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - print a string recursively
- * @s: string
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		_putchar('\n');
-	else
+	if (*s)
 	{
 		_putchar(*s);
-		return (_puts_recursion(s + 1));
+		_puts_recursion(s + 1);
 	}
+
+	else
+		_putchar('\n');
 }
