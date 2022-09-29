9a2029a44455224755a968529837fee4337ead97
Idris Oladosu <idrisoladosu1@gmail.com>
2022-09-29 06:31:34 -0700

    recursiono

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print string in reverse recursively
+ * @s: string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		_print_rev_recursion(s);
+		putchar(*s);
+	}
+}
