5907cac0e358d465c12fe83558e2639851300d8f
Idris Oladosu <idrisoladosu1@gmail.com>
2022-09-30 02:02:52 -0700

    recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.c"
+
+/**
+ * _pow_recursion - print the power of int
+ * @x: int to be raised
+ * @y: factor to be raised by
+ * Return: the x to power y and -1 if y is negative
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, (y - 1)));
+}
+
