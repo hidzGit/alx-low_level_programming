4a1f572f1279c47be672b721424d5161c721e9b1
Idris Oladosu <idrisoladosu1@gmail.com>
2022-10-01 13:55:58 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int i = 1, len = 0, boool = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for(i = 1; i > 0; i++)
+	{
+		if (*argv[i] <= '/' || *argv[i] >= ':')
+		{
+			boool = 1;
+			printf("Error\n");
+			return (1);
+		}
+	}
+	if (boool == 0)
+	{
+		i = 0;
+		while (i < argc)
+		{
+			len += atoi(argv[i]);
+			i++;
+		}
+		printf("%d", len);
+	}
+	return (0);
+
+}
