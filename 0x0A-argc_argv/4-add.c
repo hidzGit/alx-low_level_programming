4db1a4347aa64d2f5cb89466e583ef93ff36a6cd
Idris Oladosu <idrisoladosu1@gmail.com>
2022-10-01 13:59:04 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * main - add arguments ints
+ * @argc: length of argument
+ * @argv: string of arg
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	int i = 1, len = 0, boool = 0;
@@ -10,7 +17,7 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-	for(i = 1; i > 0; i++)
+	for (i = 1; i > 0; i++)
 	{
 		if (*argv[i] <= '/' || *argv[i] >= ':')
 		{
