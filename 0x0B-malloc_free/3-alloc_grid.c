388977c23b8a43625f40163f5a0e38595e08ffe9
Idris Oladosu <idrisoladosu1@gmail.com>
2022-10-06 15:43:19 -0700

    task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * alloc_grid - create 2d array using malloc
+ * @width: array width
+ * @height: array height
+ * Return: the array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid, x, y;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (y = 0; y < height; y++)
+	{
+		grid[y] = malloc(sizeof(int) * width);
+		if (grid[y] == NULL)
+		{
+			free(grid[y]);
+			free(grid);
+			return (NULL);
+		}
+
+		for (x = 0; x < width; x++)
+		{
+			grid[y][x] = 0;
+		}
+	}
+	return (grid);
+}
