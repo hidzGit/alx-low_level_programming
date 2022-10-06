07281f63f27dfb078e7eb337a91cf92a0fbdbd56
Idris Oladosu <idrisoladosu1@gmail.com>
2022-10-05 22:37:49 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,26 +1,37 @@
 #include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - return the copy of the inputted string using malloc
+ * @str: inputted str
+ * Return: null if malloc is unsuccessful but the string if successful
+ */
 
 char *_strdup(char *str)
 {
 	char *n_str;
-	int i = 0, x = 0;
+	int i, x = 0;
 
 	if (str == NULL)
 		return (NULL);
-	while (str[i])
+
+	for (i = 0; str[i]; i++)
 	{
-		i++;
+		x++;
 	}
-	i = i + 1;
-	n_str = malloc(sizeof(char) * i);
+
+	n_str = malloc(sizeof(char) * (x + 1));
+
 	if (n_str == NULL)
 		return (NULL);
-	while (x < i)
+
+	i = 0;
+	while (str[i])
 	{
-		str[x] = n_str[x];
-			x++;
+		n_str[i] = str[i];
+			i++;
 	}
-	n_str[i] = '\0';
+	n_str[x] = '\0';
 
 	return (n_str);
 }
